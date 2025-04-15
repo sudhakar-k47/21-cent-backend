8ab2d296f21cd873e0dc29609a1ba881f373855e
sudhakar-k47 <sudhakarmaruthu@gmail.com>
2025-04-15 20:33:47 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/next', function () {
+    return response()->json('message', 'Next Hello World!');
+});
